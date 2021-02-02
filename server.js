bf25f17b9622f02ff7d447015375557b7c50f129
ReaganG <rbgrunwald1997@gmail.com>
2021-02-02 10:03:21 -0600

    heroku port fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
