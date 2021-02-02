1c3daab3c24b8cead6a4984b772d11d2a8be1172
ReaganG <rbgrunwald1997@gmail.com>
2021-02-02 10:20:16 -0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('../noteTaker/db/db.json');
+const db = require('db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
